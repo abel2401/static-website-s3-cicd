8295ee5c6fe99833b95e9e71cb3fda7cdffe22bb
Abel G <abel.achucho@gmail.com>
2023-02-07 20:40:35 +0000

    del

--- apply-terraform.sh
+++ apply-terraform.sh
@@ -10,7 +10,7 @@ cd terraform-s3-website-master/live/abel-s3-website
 terraform init
 
 #apply #terraform
-terraform apply -auto-approve
+#terraform apply -auto-approve
 
 #destroy terraform
-#terraform destroy -auto-approve
+terraform destroy -auto-approve
